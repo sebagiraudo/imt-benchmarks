8848799a1254b8f155a8a516935df910e20b9e6d
Sebastian Giraudo <seba.jose.giraudo@gmail.com>
2024-09-24 09:26:57 -0300

    chore: change hash function to be the same in Rust as in zk kit implementation.

--- imt-poseidon-rs/src/hash.rs
+++ imt-poseidon-rs/src/hash.rs
@@ -1,14 +1,20 @@
 use light_poseidon::{ Poseidon, PoseidonHasher };
 use ark_bn254::Fr;
-use ark_ff::PrimeField;
+use num_bigint::BigUint;
+
+fn string_to_biguint(num_str: &str) -> BigUint {
+    num_str
+        .parse()
+        .expect("Failed to parse the string into BigUint")
+}
 
 pub fn hash_function(nodes: Vec<String>) -> String {
     let mut poseidon = Poseidon::<Fr>::new_circom(2).unwrap();
 
-    let input1 = Fr::from_be_bytes_mod_order(nodes[0].as_bytes());
-    let input2 = Fr::from_be_bytes_mod_order(nodes[1].as_bytes());
+    let input1 = Fr::from(string_to_biguint(&nodes[0]));
+    let input2 = Fr::from(string_to_biguint(&nodes[1]));
 
     let hash = poseidon.hash(&[input1, input2]).unwrap();
 
-    return hash.to_string();
+    hash.to_string()
 }
